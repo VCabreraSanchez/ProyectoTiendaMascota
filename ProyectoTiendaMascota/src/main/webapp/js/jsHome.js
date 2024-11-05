bb7985d22164a03a8573f483cf5c8c466ccbc9ed
VCabreraSanchez <cabrerasanchezv11@gmail.com>
2024-11-04 19:04:31 -0500

    agregando listado de productos y pie de pagina, y agregando archivo detalleProducto

--- ProyectoTiendaMascota/src/main/webapp/js/jsHome.js
+++ ProyectoTiendaMascota/src/main/webapp/js/jsHome.js
@@ -9,3 +9,15 @@ function updateUserStatus(username) {
 
 // Ejemplo de uso:
 //updateUserStatus('usuarioEjemplo'); // Llama a esta función cuando el usuario inicie sesión
+
+function sortProducts() {
+    const order = document.getElementById('orderSelect').value;
+    // Lógica para ordenar productos en función del valor de "order"
+    if (order === 'asc') {
+        // Implementa la lógica para ordenar de forma ascendente
+        console.log("Ordenar Ascendente");
+    } else if (order === 'desc') {
+        // Implementa la lógica para ordenar de forma descendente
+        console.log("Ordenar Descendente");
+    }
+}
